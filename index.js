9ab55bc945ef15aee1a497e0084f5386c511ea41
Swagam Dasgupta <swagam.dasgupta@gmail.com>
2022-10-06 22:04:32 +0530

    Initial files

---
+++ index.js
@@ -0,0 +1,28 @@
+
+const characters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9","~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?",
+"/"];
+
+// let characters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9","~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?",
+// "/"]
+
+passwordOneEl = document.getElementById("password-one")
+passwordTwoEl = document.getElementById("password-two")
+
+
+function getPassword(){
+    let newPassword = ""
+    // console.log(newPassword)
+    for (let count=0; count<15; count++){
+        randomIndex = Math.floor(Math.random() * characters.length)
+        newPassword += characters[randomIndex]
+    }
+    // console.log(newPassword)
+    return newPassword
+}
+
+function buttonClick(){
+    passwordOneEl.textContent = getPassword()
+    passwordTwoEl.textContent = getPassword()
+}
+
+
